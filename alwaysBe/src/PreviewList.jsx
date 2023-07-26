d19908fc0d3937d310f46a80681a25bd03faf882
Adolfo J. Temoche Guevara <ajt21397@hotmail.com>
2023-07-26 16:28:39 -0700

    reverted back to original, had issues

--- alwaysBe/src/PreviewList.jsx
+++ alwaysBe/src/PreviewList.jsx
@@ -1,11 +1,12 @@
+// PreviewList.js
 import React from 'react';
 import './index.css';
 
 const PreviewList = ({ items, onItemClick }) => {
   return (
     <ul className="preview-list">
-      {items.map((item, index) => (
-        <li key={item.url || index} onClick={() => onItemClick(item)}>
+      {items.map(item => (
+        <li key={item.url} onClick={() => onItemClick(item)}>
           {/* Display the preview content for each planet */}
           <h3>{item.name}</h3>
           <p>Climate: {item.climate}</p>
